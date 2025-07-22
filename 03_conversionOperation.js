912676a9c02356628816118c78027bb387e2f36d
Sankhadip_Manna <sankhadipmanna10@gmail.com>
2025-07-22 18:23:21 +0530

    conversionOperation and Data types

---
+++ 03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "sankha"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// 33abc => NaN // Full From => Not an Number
+ // true => 1; false => 0;
+
+ let isLoggedIn = "sankha"
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof boolenIsLoggedIn);
+console.log(boolenIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "sankha" => true;
+
+
+let someNumber = 33
+
+let stringNuber = String(someNumber)
+console.log(stringNuber);
+console.log(typeof stringNuber);
