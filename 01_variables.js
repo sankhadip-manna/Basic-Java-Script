905ef2d2ccfe104924dba08859e0c4d4feb36ea1
Sankhadip_Manna <sankhadipmanna10@gmail.com>
2025-07-22 17:50:30 +0530

    java script basic

---
+++ 01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1455678
+let accountEmail = "Sankhadipmanna@gmail.com"
+var accountPassword = ""
+accountCity = "Howrah"
+ let accountSate;
+
+// accountId = 1 // not alloowed 
+/*
+prefer not to use var 
+ because of issue in block scope and functional scoe 
+*/
+
+accountEmail = "sankha@.com"
+accountPassword = "2334455667"
+accountCity  ="Kolkata"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountSate]);
+
---
+++ 02_data-types.js
@@ -0,0 +1,32 @@
+"use strict"//treat all js code as never version
+
+// alert( 3+ 3) // We are using node js , not browser
+
+// console.log(3+
+//     3); // code readability should be high
+
+    console.log("Sankha");
+
+    let name = "Sankha"
+    let age = 34
+    let isLoggedIn = false
+let state = null
+
+    // number => 2 to power 53 
+    // bigint
+    // boolen => false/true
+    //null => standalone value
+    //undifine =>
+        // symboll => unique
+
+
+    // object =>
+
+        console.log( typeof "Sankha");
+
+        console.log(typeof null); // object
+
+
+        console.log(typeof undefined); //undefined
+        
+        
