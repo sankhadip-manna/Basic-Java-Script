d475a8120c5102acc6b5d4297f347b6ad2d9ac3a
Sankhadip_Manna <sankhadipmanna10@gmail.com>
2025-07-31 10:42:53 +0530

    Add new flie

---
+++ 03-basic/03-ArrowFunction.js
@@ -0,0 +1,50 @@
+const usre = {
+    usrename: "Sankha",
+    Usreid: 1324524,
+    price:999,
+    WelComeMassage: function(){
+        console.log(`${this.usrename}, Welcome To Website`)
+        // console.log(this)
+    }
+}
+// usre.WelComeMassage()
+// usre.usrename = "sam"
+// usre.WelComeMassage()
+// console.log(this)
+
+// function well (){
+//     console.log(this)
+// }
+// well()
+
+// const chai = function(){
+//     let usrename="Snakha"
+//     console.log(this.usrename)
+// }
+// chai()
+const chai = ()=>{
+    let usrename="Snakha"
+    console.log(this)
+}
+// chai()
+
+
+
+// const addtwo = (num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addtwo(2,4))
+// const addtwo = (num1,num2)=> num1+num2
+
+
+// const addtwo = (num1,num2) => (num1+num2)
+
+
+
+const addtwo = (num1,num2) => ({usrename:"Sankha"})
+
+
+
+console.log(addtwo(2,4))
+
+
---
+++ 03-basic/04-immediately-invoked-function.js
@@ -0,0 +1,7 @@
+// Immediately Invoked Function Expression (IIFE)
+
+
+function chai(){
+    console.log("DB CONNECTED")
+}
+chai()
