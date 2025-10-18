55e0c94b57017c5a0d27aa35ac51f70b1617923a
sankhadip-manna <sankhadipmanna10@gmail.com>
2025-10-18 20:08:52 +0530

    Add New Line In Data Types

--- comparision-of-datatypes.js
+++ comparision-of-datatypes.js
@@ -19,3 +19,5 @@ console.log(undefined > 0);
 // ===
 
 console.log("2" === 2);
+// The reson is that an equlity cheak == and comparisons > < >= <= work differenty . 
+// Comparisons convart null to a number, treating it as 0. That's why (3)null>=0 is true and (1)null > 0 is false
